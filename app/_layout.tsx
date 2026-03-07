4292a3a0913fa5b4352d516978ab3be68b1ba9e6
Ahmed alzaid <ahmedalzaid981@gmail.com>
2026-03-07 07:38:52 +0300

    Add navbar footer and pages UI

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,39 @@
-import { Stack } from "expo-router";
+import { Stack, usePathname } from "expo-router";
+import { StyleSheet, View } from "react-native";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 export default function RootLayout() {
+  const pathname = usePathname();
+
+  const hideBars = pathname === "/" || pathname === "/register";
+
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="register" />
-      <Stack.Screen name="(tabs)" />
-    </Stack>
+    <View style={styles.container}>
+      {!hideBars && <Navbar userName="Yasir Fahad" />}
+
+      <View style={styles.content}>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="register" />
+          <Stack.Screen name="about" />
+          <Stack.Screen name="homePage" />
+          <Stack.Screen name="adminLogs" />
+<Stack.Screen name="childLog" />
+        </Stack>
+      </View>
+
+      {!hideBars && <Footer />}
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#EEF1EF",
+  },
+  content: {
+    flex: 1,
+  },
+});
